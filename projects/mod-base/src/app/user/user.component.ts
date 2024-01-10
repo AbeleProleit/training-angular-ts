09b9932e829c686a48c38c2d84a598da9ed53bf6
AbeleProleit <145967984+AbeleProleit@users.noreply.github.com>
2024-01-10 12:49:22 +0100

    first components

---
+++ projects/mod-base/src/app/user/user.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pl-user',
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss',
+})
+export class UserComponent {
+  name = 'Em';
+  private age = 22;
+  get displayAge(): string {
+    if (this.age < 38) return this.age.toString();
+    if (this.age === 42) return 'nice';
+    if (this.age < 45) return 'having a midlife crisis';
+    if (this.age === 69) return 'nice';
+    return 'getting pretty darn old';
+  }
+  experienceAgeing() {
+    this.age += 1;
+  }
+}
